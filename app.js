be800d828139f509fa72566eaaa3c3d3e0ab29e1
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-01 16:22:42 +0530

    Basic auth using passport

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
@@ -5,3 +7,89 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		// listen for requests
+		app.listen(process.env.PORT, () => {
+			console.log("connected to db & listening on port", process.env.PORT);
+		});
+	})
+	.catch((err) => console.error(err));
+
+const User = mongoose.model(
+	"User",
+	new Schema({
+		username: { type: String, required: true },
+		password: { type: String, required: true },
+	})
+);
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index", { user: req.user }));
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.post("/sign-up", async (req, res, next) => {
+	try {
+		const user = new User({
+			username: req.body.username,
+			password: req.body.password,
+		});
+		const result = await user.save();
+		res.redirect("/");
+	} catch (err) {
+		return next(err);
+	}
+});
+app.post(
+	"/log-in",
+	passport.authenticate("local", {
+		successRedirect: "/",
+		failureRedirect: "/",
+	})
+);
+app.get("/log-out", (req, res, next) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+		res.redirect("/");
+	});
+});
+
+passport.use(
+	new LocalStrategy(async (username, password, done) => {
+		try {
+			const user = await User.findOne({ username: username });
+			if (!user) {
+				return done(null, false, { message: "Incorrect username" });
+			}
+			if (user.password !== password) {
+				return done(null, false, { message: "Incorrect password" });
+			}
+			return done(null, user);
+		} catch (err) {
+			return done(err);
+		}
+	})
+);
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
